7ccd513f8320099b7cd67b7a37c78e21ec9b5a20
manuhortet <manuhortet@gmail.com>
2017-12-16 18:51:41 +0100

    Edited the read function. Now the boot can read more than one book in each training session.

--- generator.py
+++ generator.py
@@ -8,24 +8,28 @@ tweetbot = MarkovBot()
 # Log in to Twitter
 # tweetbot.twitter_login(consumer_key, consumer_secret, access_token, access_token_secret)
 # Construct the path to the book
-book = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'LaNausea-Sartre.txt')
-# Make your bot read the book!
-tweetbot.read(book)
+books = [os.path.join(os.path.dirname(os.path.abspath(__file__)), 'LaNausea-Sartre.txt'),
+         os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Critica_de_la_razon_dialectica.txt')]
 
-tweet_len = randint(5, 15)
-
-tweet = tweetbot.generate_text(tweet_len, seedword=[])
-
-tweet = "El sentido de la vida es " + tweet[0].lower() + tweet[1::]
+# Make your bot read the books!
+for book in books:
+    tweetbot.read(book)
 
 
+for x in range(0, 50):
 
-# print(tweet)
+    tweet_len = randint(5, 10)
 
-# writting to our generated_sentences.txt file
-# sentences_list = open("generated_sentences.txt", "a")
-# sentences_list.write(tweet + "\n")
-# sentences_list.close()
+    tweet = tweetbot.generate_text(tweet_len, seedword=['tiempo'])
+
+    tweet = "El sentido de la vida es " + tweet[0].lower() + tweet[1::]
+
+    # print(tweet)
+
+    # writting to our generated_sentences.txt file
+    sentences_list = open("generated_sentences.txt", "a")
+    sentences_list.write(tweet + "\n")
+    sentences_list.close()
 
 
 
