6c53d02b3ce493021d0cf17226c2fc5e6749f615
manuhortet <manuhortet@gmail.com>
2017-12-16 18:49:30 +0100

    Differentiated between poster and generator, and recoded the .txt management on poster.py

--- poster.py
+++ poster.py
@@ -2,18 +2,24 @@ from keys import *
 import time
 
 while True:
-
     # reading and removing sentences from our generated_sentences.txt file
     sentences_list = open("generated_sentences.txt", "r+")
     tweet = sentences_list.readline()
-    new_txt_file = sentences_list.readlines()
-    sentences_list.truncate(0)
-    for x in range(0, len(new_txt_file)):
+    sentences_list.close()
+
+    sentences_list = open("generated_sentences.txt", "r")
+    new_txt_file = [line for line in sentences_list]
+    sentences_list.close()
+
+    sentences_list = open("generated_sentences.txt", "w")
+    sentences_list.truncate()
+    for x in range(1, len(new_txt_file)):
         sentences_list.write(new_txt_file[x])
+    sentences_list.close()
 
     # tweeting!
     api.update_status(status=tweet)
 
     # waiting an hour to tweet again :)
-    time.sleep(1200)
+    time.sleep(3600)
 
